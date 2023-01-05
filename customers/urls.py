a8d7512944703a1eb1db72867e5f826b78149693
leumanuel12 <cantiveros12@gmail.com>
2023-01-06 02:06:20 +0800

    added customer api for checking individual customer details

--- customers/urls.py
+++ customers/urls.py
@@ -19,5 +19,6 @@ from customers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/customers/', views.customers, name='customers') #similar from routes in react
+    path('api/customers/', views.customers, name='customers'), #similar from routes in react
+    path('api/customers/<int:id>', views.customer, name='customer')
 ]
--- customers/views.py
+++ customers/views.py
@@ -2,7 +2,14 @@ from  customers.models import Customer
 from django.http import JsonResponse
 from customers.serializers import CustomerSerializer
 
+#this will get all customers and details
 def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
+    return JsonResponse({'customer': serializer.data})
+
+#this will get individual customer details per id
+def customer(request, id):
+    data = Customer.objects.get(id)
+    serializer = CustomerSerializer(data)
     return JsonResponse({'customer': serializer.data})
