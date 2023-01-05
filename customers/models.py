ccba8864c523b68daec43bfb3afbda4a09e76cfb
leumanuel12 <cantiveros12@gmail.com>
2023-01-05 12:03:12 +0800

    done initial basic setup of database models, serializers, routes and views

---
+++ customers/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    industry = models.CharField(max_length=100)
---
+++ customers/views.py
@@ -0,0 +1,8 @@
+from  customers.models import Customer
+from django.http import JsonResponse
+from customers.serializers import CustomerSerializer
+
+def customers(request):
+    data = Customer.objects.all()
+    serializer = CustomerSerializer(data, many=True)
+    return JsonResponse({'customer': serializer.data})
