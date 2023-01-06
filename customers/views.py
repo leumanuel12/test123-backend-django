f9548c8bff0b3e9ab986fb023a1359b77b93c35f
leumanuel12 <cantiveros12@gmail.com>
2023-01-06 10:33:48 +0800

    tweak only typo/spacings

--- customers/views.py
+++ customers/views.py
@@ -5,9 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+
 # this will get all customers and details
-
-
 @api_view(['GET', 'POST'])
 def customers(request):
     if request.method == 'GET':
